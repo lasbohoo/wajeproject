95a169e70b9330df910f0cdbd329706e3b941e35
lasbohoo <temitopefaluta@gmail.com>
2022-07-16 13:21:29 +0100

    req

--- wajeProject/settings.py
+++ wajeProject/settings.py
@@ -167,6 +167,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 django_heroku.settings(locals())
 
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
